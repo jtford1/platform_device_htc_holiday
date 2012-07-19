d3254b1809a2d1d542f487d0ef4385dd3842cb6a
synergydev <synergycomputing@gmail.com>
2012-07-19 11:18:22 -0700

    jellybean update

--- device_holiday.mk
+++ device_holiday.mk
@@ -40,7 +40,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     device/htc/holiday/prebuilt/root/init.holiday.rc:root/init.holiday.rc \
     device/htc/holiday/prebuilt/root/init.holiday.usb.rc:root/init.holiday.usb.rc \
-    device/htc/holiday/prebuilt/root/ueventd.holiday.rc:root/ueventd.holiday.rc
+    device/htc/holiday/prebuilt/root/ueventd.holiday.rc:root/ueventd.holiday.rc \
+    device/htc/holiday/prebuilt/root/fstab.holiday:root/fstab.holiday
 
 # HTC BT Audio tune
 PRODUCT_COPY_FILES += device/htc/holiday/configs/AudioBTID.csv:system/etc/AudioBTID.csv
@@ -178,7 +179,8 @@ PRODUCT_COPY_FILES += \
 #
 # media config xml file
 PRODUCT_COPY_FILES += \
-    device/htc/holiday/configs/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/holiday/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/htc/holiday/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -201,6 +203,9 @@ PRODUCT_COPY_FILES += \
 
 # media profiles and capabilities spec
 $(call inherit-product, device/htc/holiday/media_a1026.mk)
+
+# call dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # htc audio settings
 $(call inherit-product, device/htc/holiday/media_htcaudio.mk)
