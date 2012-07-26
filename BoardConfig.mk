c9e78fd3fa82161bc846f298b82fbff497551930
synergydev <synergycomputing@gmail.com>
2012-07-26 12:41:04 -0700

    cleanup recovery custom graphics

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,4 +77,4 @@ BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_CUSTOM_GRAPHICS:= ../../../device/htc/holiday/graphics.c
+BOARD_CUSTOM_GRAPHICS:= ../../../device/htc/holiday/recovery/graphics.c
